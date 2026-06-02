3f529a6a13494b548296f695bbbd50abd177f334
KGxLinux <kgxlinux@gmail.com>
2026-06-02 23:55:17 +0530

    Add files via upload

---
+++ Array/2D Array/Print Elements In Wave Form.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+using namespace std;
+
+// Print Elements In Wave Form
+
+int main(){
+
+int arr[4][4] = {3,6,4,2,7,8,11,5,9,3,2,1,17,8,5,9};
+
+for(int j = 0 ; j < 4 ; j++) {
+
+if(j%2 == 0) {
+
+for(int i = 0 ; i < 4 ; i++) {
+
+cout << arr[i][j] << " ";
+
+}
+}
+else {
+
+for(int i = 3 ; i >= 0 ; i--) {
+
+cout << arr[i][j] << " ";
+
+}
+
+}
+
+}
+
+}
