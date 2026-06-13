135700ca4bda43513fe47278c0d14443fbf6a7aa
KGxLinux <kgxlinux@gmail.com>
2026-06-13 20:45:04 +0530

    Add files via upload

---
+++ String/Integer To Roman.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+#include <string>
+using namespace std;
+// Integer To Roman
+int main() {
+
+int num = 3458;
+int val[] = {1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
+
+string sym[] = {"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
+
+string result = "";
+
+int i = 0;
+
+while(num > 0) {
+
+  while(num >= val[i]){
+  
+    num -= val[i];
+    
+    result += sym[i];
+    
+}
+
+  i++;
+  
+}
+
+cout << result;
+
+}
