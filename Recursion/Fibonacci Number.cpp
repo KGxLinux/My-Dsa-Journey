4b8cff836dde241f018e3cd1eedfc71e4221fc7f
KGxLinux <kgxlinux@gmail.com>
2026-06-27 08:22:43 +0530

    Add files via upload

---
+++ Recursion/Fibonacci Number.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+
+// Fibonacci Number
+
+int f(int n) {
+
+if(n <= 1){
+            
+return n;
+
+}
+        
+return  f(n - 1) + f(n - 2);
+
+}
+
+int main(){
+
+cout << f(6);
+
+}
